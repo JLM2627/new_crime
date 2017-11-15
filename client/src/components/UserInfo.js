4cde1c7643765af09880164a0a01218a843b876b
Jayme Marshall <arcal619@gmail.com>
2017-11-15 15:10:49 -0500

    links on users rendering

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -40,18 +40,13 @@ class UserInfo extends Component {
       if (this.state.error) {
             return <div>{this.state.error}</div>
       }
-        return (
-        <div>
-          <h1>All Users</h1>
-        
-          {this.state.users.map(user => (
-                    <div key={user.id}>{user.name} 
-                        
-              </div>          
-          ))}
-          
-            </div>
-        )    
+        return <div>
+            <h1>All Users</h1>
+
+            {this.state.users.map(user => <div key={user.id}>
+                <Link to={`/user/${user.id}`}> {user.name} </Link>
+              </div>)}
+          </div>;    
   }
 }
 
